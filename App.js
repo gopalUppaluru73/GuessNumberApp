6aaa43d946780828809312d6a15ae10cb1cb3df5
gopalUppaluru73 <gopal.uppaluru@gmail.com>
2024-01-28 17:06:32 -0600

    App js and UserGuessInput js files updated

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Button, Text, StyleSheet } from 'react-native';
 import UserGuessInput from './app/UserGuessInput';
 
 const App = () => {
   const [hiddenNumber, setHiddenNumber] = useState(generateRandomNumber());
   const [userGuess, setUserGuess] = useState('');
   const [message, setMessage] = useState('');
-
+  const [greetMsg, setGreetMsg] = useState('')
   function generateRandomNumber() {
     return Math.floor(Math.random() * 101);
   }
@@ -18,11 +18,14 @@ const App = () => {
       setMessage('Please enter a valid number.');
     } else {
       if (userGuessNumber === hiddenNumber) {
-        setMessage('Congratulations! You have guessed it right.');
+        setGreetMsg('Congratulations!!!')
+        setMessage('You have guessed it right.');
         setHiddenNumber(generateRandomNumber());
       } else if (userGuessNumber < hiddenNumber) {
+        setGreetMsg('Try Again!!!')
         setMessage('The hidden number is higher than your guess.');
       } else {
+        setGreetMsg('Try Again!!!')
         setMessage('The hidden number is lower than your guess.');
       }
     }
@@ -32,18 +35,54 @@ const App = () => {
     setHiddenNumber(generateRandomNumber());
     setUserGuess('');
     setMessage('');
+    setGreetMsg('')
   };
 
   return (
-    <View>
-      <UserGuessInput userGuess={userGuess} setUserGuess={setUserGuess} />
-      <Button title="Check My Guess" onPress={handleCheckGuess} />
-      <Text>{message}</Text>
-      {message.includes('Congratulations') && (
-        <Button title="Play Again" onPress={handlePlayAgain} />
-      )}
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <View style={styles.container}>
+        <UserGuessInput userGuess={userGuess} setUserGuess={setUserGuess} />
+        <View style={styles.btnstyles}>
+          <Button title="Check My Guess" onPress={handleCheckGuess} color='#fff' />
+        </View>
+        <View style={{marginTop: 10}}>
+          <Text style={styles.text}>{greetMsg}</Text>
+          <Text style={styles.text}>{message}</Text>
+          {greetMsg == 'Congratulations!!!' && (
+            <View style={styles.btnstyles}>
+                <Button title="Play Again..." onPress={handlePlayAgain} color='#fff'/>
+            </View>
+          )}
+        </View>
+      </View>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    padding: 30,
+    height: 400,
+    borderWidth: 2,
+    borderColor: '#182741',
+    margin: 30
+  },
+  text: {
+    textAlign:'center',
+    marginTop: 15,
+    fontSize: 20
+  },
+  btnstyles: {
+    width: "100%",
+    height: 60,
+    backgroundColor: '#4a73c4',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    marginTop: 16
+  }
+
+});
 
 export default App;
--- app/UserGuessInput.js
+++ app/UserGuessInput.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 const UserGuessInput = ({ userGuess, setUserGuess }) => {
   return (
-    <View style={{margin: 120}}>
-      <Text>Enter a Number</Text>
+    <View>
       <TextInput
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        placeholder='Enter a Number'
+        style={styles.input}
         keyboardType="numeric"
         value={userGuess}
         onChangeText={(text) => setUserGuess(text)}
@@ -14,5 +14,10 @@ const UserGuessInput = ({ userGuess, setUserGuess }) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: { height: 50, borderColor: 'gray', borderWidth: 2, borderRadius: 8, width: '100%', padding: 10, marginBottom: 15, borderColor: '#182741', fontSize: 20 }
+
+});
 
 export default UserGuessInput;
